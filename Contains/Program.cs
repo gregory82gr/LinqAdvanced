5783de0ea3acfb8337cad415f8c86a175a5de2d5
Agathangelidis Grigorios <gregory82gr@yahoo.gr>
2025-02-22 16:13:56 +0200

    Contains

---
+++ Contains/Program.cs
@@ -0,0 +1,29 @@
+﻿using Data;
+using System.IO.Pipes;
+using System.Linq;
+
+List<Student> students = Student.GetStudents();
+List<Pet> pets = Pet.GenerateMockData();
+List<PetOwner> petOwners = PetOwner.GenerateMockData();
+
+BasingObjects.ShowDataAndRelationships(pets, petOwners, students);
+
+
+Console.WriteLine("-------------------------");
+
+bool isNemoPresentVersion1 = pets.Contains(
+    new Pet(4, "Nemo", PetType.Fish, 0.3)
+    );
+Console.WriteLine("isNemoPresentVersion1:{0}", isNemoPresentVersion1);
+
+bool isNemoPresentVersion2 = pets.Contains(
+    pets[3]
+    );
+Console.WriteLine("isNemoPresentVersion2:{0}", isNemoPresentVersion2);
+
+
+bool isNemoPresentCustomComparerVersion1 = pets.Contains(
+    new Pet(4, "Nemo", PetType.Fish, 0.3),
+    new PetComparerById()
+    );
+Console.WriteLine("isNemoPresentCustomComparerVersion1:{0}", isNemoPresentCustomComparerVersion1);
---
+++ Data/PetComparerById.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class PetComparerById : IEqualityComparer<Pet>
+    {
+        bool IEqualityComparer<Pet>.Equals(Pet? x, Pet? y)
+        {
+            return x.Id==y.Id;
+        }
+
+        int IEqualityComparer<Pet>.GetHashCode(Pet obj)
+        {
+            return obj.Id;
+        }
+    }
+}
