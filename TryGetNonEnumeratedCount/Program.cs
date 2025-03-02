c208a8142604eb1ef92a9f8e7225f50dcab0e567
Agathangelidis Grigorios <gregory82gr@yahoo.gr>
2025-03-02 16:33:22 +0200

    TryGetNonEnumeratedCount

---
+++ TryGetNonEnumeratedCount/Program.cs
@@ -0,0 +1,10 @@
+﻿List<int> numbers = new List<int>(Enumerable.Range(1, 1000));
+if (numbers.TryGetNonEnumeratedCount(out int count))
+{
+    Console.WriteLine("Number of elements is {0}", count);
+}
+else
+{
+    Console.WriteLine("could not retrieve count without enumeration");
+}
+
