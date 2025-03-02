77008174139edf635da67e49c7bf8370772a0ea3
Agathangelidis Grigorios <gregory82gr@yahoo.gr>
2025-03-02 16:58:56 +0200

    ToHashSet()

---
+++ ToHashset/Program.cs
@@ -0,0 +1,11 @@
+﻿List<int> numbers = new List<int> { 5, 2, 3, 4, 1, 2, 3, 4 };
+
+// Convert to HashSet using ToHashSet()
+HashSet<int> uniqueNumbers = numbers.ToHashSet();
+
+// Display the unique numbers
+Console.WriteLine("Unique numbers:");
+foreach (int number in uniqueNumbers)
+{
+    Console.WriteLine(number);
+}
