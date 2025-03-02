a4f3cc65f561664f50eff5de4f005bc629cd8b56
Agathangelidis Grigorios <gregory82gr@yahoo.gr>
2025-03-02 16:25:12 +0200

    chunk

---
+++ Chunk_Range/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+
+var numbers = new List<int>(Enumerable.Range(1, 25));
+var chunks = numbers.Chunk(10);
+
+Console.WriteLine("Chunks  of 10 numbers");
+foreach (var chunk in chunks)
+{
+    foreach (var item in chunk)
+    {
+        Write($"{item}, ");
+    }
+    WriteLine();
+}
