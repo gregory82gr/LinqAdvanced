bc12b298d7d6d758073590670e587cfaea957d63
Agathangelidis Grigorios <gregory82gr@yahoo.gr>
2025-03-03 11:37:50 +0200

    ToArray,Cast<T>

---
+++ Cast/Program.cs
@@ -0,0 +1,17 @@
+﻿// An object array where each element is an integer (boxed as object)
+object[] numberObjects = { 10, 20, 30, 40, 50 };
+
+// Use Cast<int>() to convert the object array to IEnumerable<int>
+var numbers = numberObjects.Cast<int>();
+
+// Use LINQ operations on the converted collection
+int sum = numbers.Sum();
+Console.WriteLine($"Sum: {sum}");
+
+// Filter the numbers to get only those greater than 25
+var filteredNumbers = numbers.Where(n => n > 25);
+Console.WriteLine("Numbers greater than 25:");
+foreach (var number in filteredNumbers)
+{
+    Console.WriteLine(number);
+}
---
+++ ToArray/Program.cs
@@ -0,0 +1,11 @@
+﻿var numbers = Enumerable.Range(1, 10);
+
+// Use LINQ to filter even numbers and convert the result to an array
+var evenNumbersArray = numbers.Where(n => n % 2 == 0).ToArray();
+
+// Display the array
+Console.WriteLine("Even Numbers:");
+foreach (var number in evenNumbersArray)
+{
+    Console.WriteLine(number);
+}
