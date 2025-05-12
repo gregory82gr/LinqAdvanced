9548b8139a956679d392cbf8cf1c024038d2bd72
Agathangelidis Grigorios <gregory82gr@yahoo.gr>
2025-05-12 18:07:52 +0300

    countby

---
+++ CountBy/Program.cs
@@ -0,0 +1,34 @@
+﻿using Data;
+
+List<Pet> pets = Pet.GenerateMockData();
+Console.WriteLine("All pets:");
+foreach (var pet in pets)
+{
+    Console.WriteLine(pet);
+}
+Console.WriteLine("---------------");
+//Count pets by groupBy and select
+var petCountByTypeOld = pets.GroupBy(pet => pet.Type)
+    .Select(group => new
+    {
+        Type = group.Key,
+        Count = group.Count()
+    });
+
+Console.WriteLine("Count pets by type using GroupBy and Select:");
+foreach (var petCount in petCountByTypeOld)
+{
+    Console.WriteLine($"Type: {petCount.Type}, Count: {petCount.Count}");
+}
+Console.WriteLine("---------------");
+//Count pets by type using CountBY
+var petCountByType = pets.CountBy(p => p.Type);
+
+Console.WriteLine("Count pets by type using CountBy:");
+
+foreach (var petCount in petCountByType)
+{
+    Console.WriteLine($"Type: {petCount.Key}, Count: {petCount.Value}");
+}
+
+
